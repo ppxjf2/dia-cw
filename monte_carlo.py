03115d9448756e9cb90a1e54cf43bfb242d68d03
ppxjf2 <ppxjf2@nottingham.ac.uk>
2023-05-15 05:32:36 +0100

    working monte carlo

--- monte_carlo.py
+++ monte_carlo.py
@@ -7,23 +7,23 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 
-pos_actions=5
+pos_actions=3
 
 class MonteCarlo:
 
     def __init__(self, env):        
-        self.pacman_x = 0
-        self.pacman_y = 0
+        self.pacman_x = 10
+        self.pacman_y = 9
         
-        self.orange_x = 0
-        self.blue_x = 0
-        self.pink_x = 0
-        self.red_x = 0
+        self.orange_x = 10
+        self.blue_x = 10
+        self.pink_x = 10
+        self.red_x = 10
         
-        self.orange_y = 0
-        self.blue_y = 0
-        self.pink_y = 0
-        self.red_y = 0
+        self.orange_y = 7
+        self.blue_y = 7
+        self.pink_y = 7
+        self.red_y = 5
         
         self.cherry_x = 0
         self.cherry_y = 0
@@ -31,14 +31,19 @@ class MonteCarlo:
         self.G = []
         self.env = env
         
-        # values= np.random.random([,pos_actions]) 
-        # self.value_function = values
-        # self.steps
+        self.current_action = 3
+        self.counter = 0
+        self.action = 0
+        self.score = 0
+        values = np.random.random([15, 21, 15, 21, pos_actions]) 
+        self.value_function = values
+        self.steps = 20000
         
         # 12y 8x is one map square 
         self.defaultMap = self.mapGen()
-        self.pillMap = self.pillGen()
-        self.ghostMap = self.mapGen()
+        self.pillMap = self.pillMapGen()
+        self.ghostMap = self.ghostMapGen()
+        self.ghostMapReset()
         
         self.graph = self.graphGen()
         
@@ -55,28 +60,34 @@ class MonteCarlo:
                         coord.append((a,b))                            
                     
                 graph[(y,x)] = coord
-                # print( str(y) + ", " + str(x) + " : " + str(coord))
-        # print(graph[(3,4)])
         
-        graph[(5,0)] = [(5,1),(5,20)]
-        graph[(9,0)] = [(9,1),(9,20)]
+        # graph[(5,0)] = [(5,1),(5,20)]
+        # graph[(9,0)] = [(9,1),(9,20)]
         
-        graph[(5,20)] = [(5,1),(5,0)]
-        graph[(9,20)] = [(9,1),(9,0)]
+        # graph[(5,20)] = [(5,19),(5,0)]
+        # graph[(9,20)] = [(9,19),(9,0)]
         
-        # for each in graph:
-        #     print(each)
+        
+        graph[(5,0)] = [(5,1)]
+        graph[(9,0)] = [(9,1)]
+        graph[(5,20)] = [(5,19)]
+        graph[(9,20)] = [(9,19)]
         
         return graph
         
     def mapGen(self):
         # board[y][x]
-        board = np.loadtxt("small_map_test.txt", dtype="uint8", delimiter=' ')
+        board = np.loadtxt("maps/map.txt", dtype="uint8", delimiter=' ')
         return board
         
-    def pillGen(self):
+    def pillMapGen(self):
         # board[y][x]
-        board = np.loadtxt("pill_map.txt", dtype="uint8", delimiter=' ')
+        board = np.loadtxt("maps/pill_map.txt", dtype="uint8", delimiter=' ')
+        return board
+    
+    def ghostMapGen(self):
+        # board[y][x]
+        board = np.loadtxt("maps/ghost_map.txt", dtype="uint8", delimiter=' ')
         return board
 
     def value_calc(self, state):
@@ -84,18 +95,18 @@ class MonteCarlo:
         if (np.random.rand() > 0.99):
             return np.random.randint(pos_actions)
         else:  
-            max1 = max(self.value_function[state[0]][state[1]][state[2]][state[3]])
+            maxValue = max(self.value_function[state[0]][state[1]][state[2]][state[3]])
             i = 0
             for n in range(pos_actions):
-                if(max1 == self.value_function[state[0]][state[1]][state[2]][state[3]][n]):
+                if(maxValue == self.value_function[state[0]][state[1]][state[2]][state[3]][n]):
                     i=n
             return i
 
-    def reward(self, score):
+    def reward(self, state, ghost_dist):
         # negative if ghosts too close
         # negative on death
-               
-        reward = ghost_distance + score       
+        
+        reward = -ghost_dist + self.score
         
         return reward
     
@@ -103,96 +114,155 @@ class MonteCarlo:
         # orange 3, blue 4, pink 5, red 6
         self.ghostMap = self.defaultMap
         
-        
         self.ghostMap[self.orange_y][self.orange_x] = 3
         self.ghostMap[self.blue_y][self.blue_x] = 4
         self.ghostMap[self.pink_y][self.pink_x] = 5
         self.ghostMap[self.red_y][self.red_x] = 6
         
     def closestGhost(self):
+        route = self.bfs("ghost", self.graph, (self.pacman_y, self.pacman_x))        
+        ghost_y, ghost_x = route[-1]
 
-        visited = [] # List for visited nodes.
-        self.queue = []     #Initialize a queue
+        x = int(ghost_x)
+        y = int(ghost_y)
+        dist = len(route)
         
-        self.bfs(visited, self.graph, )        
-        
-        
-        x = 0
-        y = 0
-        
-        return x, y             
+        return y, x, dist
 
     # found solution for returning path from stack overflow answered by SeasonalShot
     # https://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
-    def bfs(self, graph, start):
-        
+    def bfs(self, map_type, graph, start):
+              
+        if(map_type == "ghost"):
+            board = self.ghostMap
+        elif(map_type == "pill"):
+            board = self.pillMap
+
+            
         queue = [(start,[start])]
-        visited = set()
+        visited = []
     
         while queue:
             vertex, path = queue.pop(0)
-            visited.add(vertex)
+            visited.append(vertex)
             
             for node in graph[vertex]:
-                if(self.ghostMap[node[0]][node[1]] != 0):
+                if(board[node[0]][node[1]] > 0):
                     return path + [node]
                 else:
                     if node not in visited:
-                        visited.add(node)
+                        visited.append(node)
                         queue.append((node, path + [node]))
                         
+        return [(1,1)]
     
-
-    def action_selection(self, states, action):
+    def state_machine(self):
+        ghost_y, ghost_x, ghost_dist = self.closestGhost()
         
-        ghost_x, ghost_y = self.closestGhost()
-        
+        # State
         state = (
-            int(self.pacman_x),
             int(self.pacman_y),
-            int(),
-            int()
+            int(self.pacman_x),
+            int(ghost_y),
+            int(ghost_x)
         )
         self.states.append(state)
         
+        action = self.action_selection()
+        
         # Action
         self.action = self.value_calc(state)
+        # self.action = 0
         self.actions.append(self.action)
 
         # Reward
-        reward = self.reward()
+        reward = self.reward(state, ghost_dist)
         self.rewards.append(reward)
+        
+        return action
 
+    def action_selection(self):
+        # pacman_y = state[0]
+        # pacman_x = state[1]   
 
-        up = self.defaultMap[self.pacman_y-1][self.pacman_x]
-        down = self.defaultMap[self.pacman_y+1][self.pacman_x]
-        left = self.defaultMap[self.pacman_y][self.pacman_x-1]
-        right = self.defaultMap[self.pacman_y][self.pacman_x+1]
-
-        if(action == 0):
-            # run from ghosts
-        
-            # if(self.ghost):
-        
-        
-            # if(up == 0):
-            
-            
-            
-            return 1
-        
-        elif(action == 1):
+        if(self.action == 0):
             # collect pills
+            route = self.bfs("pill", self.graph, (self.pacman_y, self.pacman_x))
             
-            return 1
-        
-        elif(action == 2):
-            # chase blue ghosts 
             
-            return 1
-
+            if(len(route) != 1):
+                direction = route[1]
+         
+                y = direction[0] - self.pacman_y
+                x = direction[1] - self.pacman_x
+            else:
+                x = 1
+                y = 1
+            
+            
+            if(x != 0):
+                if(x > 0):
+                    action = 2 # right
+                else: 
+                    action = 3 # left
+            
+            elif(y !=0):
+                if(y > 0):
+                    action = 4 # down
+                else: 
+                    action = 1 # up
         
+        elif(self.action == 1):
+            # run from ghosts    
+            
+            route = self.bfs("ghost", self.graph, (self.pacman_y, self.pacman_x))
+            if(len(route) != 1):
+                direction = route[1]
+         
+                y = direction[0] - self.pacman_y
+                x = direction[1] - self.pacman_x
+            else:
+                x = 1
+                y = 1
+            
+            if(x != 0):
+                if(x > 0):
+                    action = 3 # left
+                else: 
+                    action = 2 # right
+            
+            elif(y !=0):
+                if(y > 0):
+                    action = 1 # up
+                else: 
+                    action = 4 # down
+            
+            # action = 1
         
+        elif(self.action == 2):
+            # chase ghosts 
+            route = self.bfs("ghost", self.graph, (self.pacman_y, self.pacman_x))
+            if(len(route) != 1):
+                direction = route[1]
+         
+                y = direction[0] - self.pacman_y
+                x = direction[1] - self.pacman_x
+            else:
+                x = 1
+                y = 1
+            
+            if(x != 0):
+                if(x > 0):
+                    action = 2 # right
+                else: 
+                    action = 3 # left
+            
+            elif(y !=0):
+                if(y > 0):
+                    action = 4 # down
+                else: 
+                    action = 1 # up
+            
         return action
     
     def setCoordinates(self, observation):
@@ -214,17 +284,14 @@ class MonteCarlo:
         
 
     def train(self, epochs):
-        # self.values= np.random.random([20,40,30,10,pos_actions]); 
+        self.values= np.random.random([15,21,15,21,pos_actions])
         
-        # average_returns = np.empty([20,40,30,10,pos_actions])
-        # average_returns_count = np.zeros([20,40,30,10,pos_actions])
-        # Q = np.empty([20,40,30,10,pos_actions])
+        average_returns = np.empty([15,21,15,21,pos_actions])
+        average_returns_count = np.zeros([15,21,15,21,pos_actions])
+        Q = np.empty([15,21,15,21,pos_actions])
 
         total_rewards = []
-        # total_targets = []
-
-
-
+        
         # --- Code snipped provided for guidance only --- #
         for n in range(epochs):
             
@@ -235,58 +302,49 @@ class MonteCarlo:
             self.actions = []
             self.rewards = []
 
-            # 2) create a new drone simulation
-            drone = self.init_drone()
-            self.nexttarget=drone.get_next_target()
             self.counter = 1
-
+                        
             # 3) run simulation
             for j in range(self.steps):
-
-                action = self.action_selection()
+                self.pillMap[self.pacman_y][self.pacman_x] = 0
                 
+                action = self.state_machine()
+                # print(action)
+
+                # observation, reward, terminated, truncated, info = self.env.step(self.current_action)
                 observation, reward, terminated, truncated, info = self.env.step(action)
                 self.setCoordinates(observation)
-                               
-                self.pillMap[self.pacman_y][self.pacman_x] = 2
                 
-                fitness += reward
-
+                
+                self.score = reward
+                
                 if terminated or truncated:
                     observation, info = self.env.reset()
                     break            
-                
-                
-                
-            
-            # for t in range(self.get_max_simulation_steps()):
-            #     drone.set_thrust(self.get_thrusts(drone))
-            #     drone.step_simulation(self.get_time_interval())
-            
-            # states, actions, rewards = self.states, self.actions, self.rewards
+                                        
+            states, actions, rewards = self.states, self.actions, self.rewards
 
-            # # 4) measure change in quality
-            # total_new_rewardsblip = np.sum(rewards)
-            # print(n, total_new_rewardsblip,self.counter)
-            # total_rewards.append(total_new_rewardsblip)
-            # total_targets.append(self.counter)
+            # 4) measure change in quality
+            total_new_rewardsblip = np.sum(rewards)
+            print(n, total_new_rewardsblip,self.counter)
+            total_rewards.append(total_new_rewardsblip)
 
-            # G = 0
-            # # 5) update parameters according to algorithm
-            # for i in reversed(range(len(actions))):
-            #     k = len(actions)-1
-            #     total_new_rewardsblip
-            #     G = np.average(rewards[(k-i):])
+            G = 0
+            # 5) update parameters according to algorithm
+            for i in reversed(range(len(actions))):
+                k = len(actions)-1
+                total_new_rewardsblip
+                G = np.average(rewards[(k-i):])
 
-            #     cur_reward = self.value_function[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
+                cur_reward = self.value_function[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
 
-            #     average_returns[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] += G
-            #     average_returns_count[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] += 1
+                average_returns[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] += G
+                average_returns_count[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] += 1
 
-            #     Q[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] = average_returns[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] / average_returns_count[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
+                Q[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] = average_returns[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] / average_returns_count[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
 
 
-            #     self.value_function[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] = Q[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
+                self.value_function[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]] = Q[states[k-i][0]][states[k-i][1]][states[k-i][2]][states[k-i][3]][actions[k-i]]
 
         pass
         
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import math
 
 from monte_carlo import MonteCarlo
 
-env = gym.make("MsPacman-ramDeterministic-v4", render_mode="human", obs_type="ram")
+env = gym.make("MsPacman-ramDeterministic-v4", render_mode="rgb_array", obs_type="ram")
 env.reset()
 
 action = np.loadtxt("best_brain.txt", dtype="uint8", delimiter=' ')
@@ -15,44 +15,27 @@ rewardSum = 0
 lastObservation = []
 
 test = MonteCarlo(env)
-# map = test.mapGen()
+test.train(1000)
 
-# visited = []
-# test.ghostMapReset()
-# print(test.red_y)
-# print(test.pacman_y)
-
-
-# print(test.ghostMap)
-
-# test.bfs(visited, test.graph, (1,1))
-
-# print(test.mapGen())
-
-# movement starts at 66 frames
-for i in range(1):
+# # movement starts at 66 frames
+# for i in range(1):
     
-    observation, reward, terminated, truncated, info = env.step(1)
-    test.setCoordinates(observation)
-
-    #pacman_x = math.floor((observation[10]+2)/8)-1
-    #pacman_y = math.floor((observation[16]+10)/12)
+#     observation, reward, terminated, truncated, info = env.step(1)
+#     test.setCoordinates(observation)
         
-    map = test.mapGen()
-    test.ghostMapReset()
+#     map = test.mapGen()
+#     test.ghostMapReset()
 
-    print(test.ghostMap)
-    print(test.bfs(test.graph, (test.pacman_y, test.pacman_x)))
-
-
-    # print(map[pacman_y][pacman_x])z
+#     print(test.pillMap)
+#     route = test.bfs("pill", test.graph, (test.pacman_y, test.pacman_x))
+#     print(route[-1])
+       
     
-    
-    # rewardSum += reward
+#     # rewardSum += reward
 
-    if terminated or truncated:
-        observation, info = env.reset()
-        # print(observation)
-        break   
+#     if terminated or truncated:
+#         observation, info = env.reset()
+#         # print(observation)
+#         break   
 
 env.close()
